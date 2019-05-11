8593d8c351ec3301db26f5a4b66ab37b1dc47e11
Desirea Holton <dezholton@gmail.com>
2019-05-11 16:06:48 +0100

    Code and comment for lesson 4.18

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+/* 4.18: The "-=" does the same thing as "+=" but with subtraction instead of addition. */
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
