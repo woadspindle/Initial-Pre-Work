f996d2b75980ed6f0a0286452ad23df5f872c1a7
Desirea Holton <dezholton@gmail.com>
2019-05-11 15:08:46 +0100

    Code and comment for lesson 4.3

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,10 @@
+/* 4.3: Values are stored in variables by using an assignment operator (=).
+Assignment occurs from right to left meaning that the information on the right side of the operator is interpreted before the final value is assigned to the variable. */
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
