526aa3acfed6faefcf316d73772146649e9c1f04
Desirea Holton <dezholton@gmail.com>
2019-05-11 22:29:54 +0100

    Code and comment for lesson 4.22

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+/*4.22: Backslashes before and after a quote inside of a string will escape the quote and keep it from being misinterpreted by the browser. */
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
