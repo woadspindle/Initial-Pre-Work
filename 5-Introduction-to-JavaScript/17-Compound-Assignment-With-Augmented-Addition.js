79e29e7318c3e49a5e0df33b1e5afadd34f62286
Desirea Holton <dezholton@gmail.com>
2019-05-11 16:03:59 +0100

    Code and comment for lesson 4.17

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+/* 4.17: The "+=" operator adds a value and assigns it to the variable in one step. */
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+a += 12;
+b += 9;
+c += 7;
