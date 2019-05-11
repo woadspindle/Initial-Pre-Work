948e3d3165e31a6cbe782ef2fa1f456e4df02c58
Desirea Holton <dezholton@gmail.com>
2019-05-11 22:40:23 +0100

    Code and comment for lesson 4.24

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+/*4.24: Escape characters allow you to use characters that can't otherwise be typed out and let you use multiple quotes without breaking the JS code. */
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
