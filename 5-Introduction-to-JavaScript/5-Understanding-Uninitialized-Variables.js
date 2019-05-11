a92bf6dc77bab4d3729d8b41e357707b1252f61c
Desirea Holton <dezholton@gmail.com>
2019-05-11 15:20:29 +0100

    Code and comment for lesson 4.5

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+/* 4.5: Uninitialized variables have a default value of "undefined". String values will give a result of "undefined", numerical ones will return NaN. */
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
