b665094b994dd1f22f5d4149e1966cd27f9c2e91
Desirea Holton <dezholton@gmail.com>
2019-05-11 16:09:54 +0100

    Code and comment for lesson 4.19

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+/*4.19: The "*=" operator multiplies and assigns a nes value to the variable. */
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
