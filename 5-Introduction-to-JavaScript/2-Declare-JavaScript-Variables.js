d1f2f318419f7c9e79d7f4485107dbe455c90b8b
Desirea Holton <dezholton@gmail.com>
2019-05-11 15:00:14 +0100

    Code and comment for lesson 4.2

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+/* THere a seven data types in JavaScript. Any of them can be stored in a variable. Variables are created in JavaScript by using the keyword var. */
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
