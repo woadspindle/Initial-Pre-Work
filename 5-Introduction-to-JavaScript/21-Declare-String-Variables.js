f922570572c8db389327cf184f58d58f27103cfc
Desirea Holton <dezholton@gmail.com>
2019-05-11 22:23:11 +0100

    Code and comment for lesson 4.21

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+/*4.21: The characters inside the quotation marks ina string data type is calle d a string literal. */
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Desirea"
+var myLastName = "Holton"
