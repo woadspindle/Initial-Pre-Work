3108d9f0e492206db9cd3fae781900c24e9c525c
Desirea Holton <dezholton@gmail.com>
2019-05-11 22:48:58 +0100

    Code and comment for lesson 4.26

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+/*4.26:You have to add the spaces into the strings you concatenate. The += operator is used to concatenate a string onto a previous string. */
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
