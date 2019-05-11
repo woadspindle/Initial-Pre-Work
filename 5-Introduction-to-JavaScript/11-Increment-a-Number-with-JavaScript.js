8fe09b5152be97725d56ae94e68810bb488a42a9
Desirea Holton <dezholton@gmail.com>
2019-05-11 15:41:51 +0100

    Code and comment for lesson 4.11

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+/*4.11: The "++" operator is the same thing as adding 1 to an existing variable. The entire operation can be shortened like this example: "aVar++;". */
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
