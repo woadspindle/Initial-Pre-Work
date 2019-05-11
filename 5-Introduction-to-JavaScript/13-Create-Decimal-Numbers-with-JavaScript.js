c0748ef792a1590bd509ffe15697709130a2700d
Desirea Holton <dezholton@gmail.com>
2019-05-11 15:48:07 +0100

    Code and comment for lesson 4.13

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+/*4.13: Decimal numbers are usually called floats or floating point numbers in computer science.  */
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
