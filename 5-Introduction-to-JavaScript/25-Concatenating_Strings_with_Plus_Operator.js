a0f42751a0bba3081be1854444624e2ce0126e25
Desirea Holton <dezholton@gmail.com>
2019-05-11 22:42:50 +0100

    Code and comment for lesson 4.25

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+/*4.25: Concatenation is when the + operator is used to combine more than one string. */
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
