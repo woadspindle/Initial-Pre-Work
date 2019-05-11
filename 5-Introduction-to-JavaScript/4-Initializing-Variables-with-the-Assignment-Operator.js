fa723ac24559f342d7490219f74914ada670a282
Desirea Holton <dezholton@gmail.com>
2019-05-11 15:13:09 +0100

    Code and comment for lesson 4.4

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,7 @@
+/* 4.4: Initializing a variable happens when you give it a starting value in the same line that you declare it.  */
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
