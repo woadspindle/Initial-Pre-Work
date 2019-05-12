b492029c1c47ab936de71b8e66738bf6bb5dfe79
Desirea Holton <dezholton@gmail.com>
2019-05-12 14:49:51 +0100

    Code and comment for lesson 4.29

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+/*4.29: You can get the number of characters in a string by appending .length to the variable name. */
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
