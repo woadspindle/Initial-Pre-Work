f889399a5e966e86d3876077d715555643e994d6
Desirea Holton <dezholton@gmail.com>
2019-05-12 14:56:36 +0100

    Code and comment for lesson 4.31

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+/*4.31: String values in JS can't be changed after you make them. They are immutable. You would have to assign it to a new string in order to edit it. */
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
