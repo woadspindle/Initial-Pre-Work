c995a2775e58b5e0d61abb38a8ff7c2b707469f5
Desirea Holton <dezholton@gmail.com>
2019-05-12 14:41:51 +0100

    Code and comment for lesson 4.27

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+/*4.27: You can use concatenation to fill in blank spaces in another strings. */
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Desirea";
+var myStr = "My name is" + myName + "and I am well!";
