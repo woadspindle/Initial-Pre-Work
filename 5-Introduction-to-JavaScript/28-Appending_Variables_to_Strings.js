12119dacf3289f7c7febd16d51765a4cbc8f0618
Desirea Holton <dezholton@gmail.com>
2019-05-12 14:46:28 +0100

    Code and comment from lesson 4.28

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+/*4.28: The += operator works on strings, as well. */
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "great.";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
