844d06c4f0151b14e9bb5018957a7da83ceba893
Desirea Holton <dezholton@gmail.com>
2019-05-12 14:58:55 +0100

    Code and comment for lesson 4.32

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+/*4.32: Bracket notation is used to ask for a character at a specific point in a string. */
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
