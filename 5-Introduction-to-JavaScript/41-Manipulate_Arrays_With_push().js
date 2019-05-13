fac53527fb8f1eaee262e1e4a93a5af2df76ea4c
Desirea Holton <dezholton@gmail.com>
2019-05-13 16:09:29 +0100

    Code and comment for lesson 4.41

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,12 @@
+/*4.41: You can add data to the end of an array by using .push(). */
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
