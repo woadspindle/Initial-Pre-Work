32a9cb747f222a9daa5ffd31c6b7c321e2a808ab
Desirea Holton <dezholton@gmail.com>
2019-05-13 15:53:35 +0100

    Code and comment for lesson 4.38

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+/*4.38: You call on data in the array by using bracket notation. The index number returns the whole amount of data between commas instead of just one character. Indexes start at the zeroth index as well. */
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
