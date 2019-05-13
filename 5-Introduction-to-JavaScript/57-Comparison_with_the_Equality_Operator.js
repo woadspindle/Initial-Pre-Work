3c0baef82fedb8e31a0274c875b72a91ce7f7d28
Desirea Holton <dezholton@gmail.com>
2019-05-13 20:49:01 +0100

    Code and comment for lesson 4.57

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+/*4.57: Comparison operators return boolean values. The equality operator(==) looks at two values and returns true if they are equal or false if they are not.  */
+
+// Setup
+function testEqual(val) {
+  if (val==12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
