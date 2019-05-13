71cf5facac9f9fdba131126e56bd7cb08c91e865
Desirea Holton <dezholton@gmail.com>
2019-05-13 17:28:36 +0100

    Code and comment for lesson 4.54

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,16 @@
+/*4.54: In a queue new items get added to the back and old items are taken off of the front. */
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
