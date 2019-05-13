c5d769bdf67dab9bdbb6b1d1e260817f1926041e
Desirea Holton <dezholton@gmail.com>
2019-05-13 16:05:51 +0100

    Code and comment for lesson 4.40

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,7 @@
+/*4.40: To access multi-dimensional arrays you call each index from the outermost inward.   */
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
