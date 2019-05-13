29399893d68b3a768ae255ecc4bf816d61526806
Desirea Holton <dezholton@gmail.com>
2019-05-13 17:13:43 +0100

    Code and comment for lesson 4.51

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+/*4.51: Values get passed into a function with arguements. The value gets sents back ouit with a return statement. */
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(a){
+  return a * 5;
+}
+
+var answer = timesFive(2);
+console.log(answer);
