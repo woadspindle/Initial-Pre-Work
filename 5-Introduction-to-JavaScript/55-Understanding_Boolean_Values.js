c71e63430efc3e256f296a4f23cabc1c38d84d20
Desirea Holton <dezholton@gmail.com>
2019-05-13 20:33:24 +0100

    Code and comment for lesson 4.55

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+/*4.55:Booleans are either true or false which can also stand for on or off. If a boolean is true it cannot be false and vice versa. */
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
