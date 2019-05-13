f3891da067d49760d907f4d3bce75f9dbf541d6a
Desirea Holton <dezholton@gmail.com>
2019-05-13 15:31:02 +0100

    Code and comment for lesson 4.37

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+/*4.37: Araays can also be put inside of other arrays. */
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["ice cream", 26], ["chocolate", "purple"]];
