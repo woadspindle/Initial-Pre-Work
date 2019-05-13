f241658e9ba0bb268bc3516f7b5e9f63acf9a9c3
Desirea Holton <dezholton@gmail.com>
2019-05-13 15:27:50 +0100

    Code and comment for lesson 4.36

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+/*4.36:In JS arrays are stored inside brackets and seperated by commas. They can contain different kinds of data types.*/
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["hello", 36];
