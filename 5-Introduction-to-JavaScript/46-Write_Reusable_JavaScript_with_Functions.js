6fc8321a5066d95b70b8bfbb05041ae63d031dea
Desirea Holton <dezholton@gmail.com>
2019-05-13 16:32:48 +0100

    Code and comment for lesson 4.46

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+/*4.46: JS code is broken into reusable sections called functions. You call a function by using its name followed by a set of parentheses. */
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
