896940da670fe282634fb1dca4c85585ced0b3df
Desirea Holton <dezholton@gmail.com>
2019-05-13 23:01:36 +0100

    Code and comment for lesson 4.69

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,13 @@
+//4.69: You can link if statements with else if statements.
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+  }}
+
+// Change this value to test
+testElseIf(7);
