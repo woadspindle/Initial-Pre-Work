e45b6a39439de89fbf841d53eac50e6037820630
Desirea Holton <dezholton@gmail.com>
2019-05-14 21:31:06 +0100

    Code and comment for lesson 4.77

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,10 @@
+/*4.77: You can use if/else statements to do a boolean comparison. The simplified version looks like this: */
+
+function isLess(a, b) {
+  // Fix this code
+  return (a <= b);
+
+}
+
+// Change these values to test
+isLess(10, 15);
