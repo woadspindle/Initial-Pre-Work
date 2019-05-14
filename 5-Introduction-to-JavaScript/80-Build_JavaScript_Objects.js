c115c17ab95d94aa2e7201adb8af846546d6eefa
Desirea Holton <dezholton@gmail.com>
2019-05-14 22:46:23 +0100

    Code and comment for lesson 4.80

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+/*4.80: Objects are like arrays, but instead of using indexes to call on their data you use properties. Objects are used to store structured data. */
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Boots",
+  "legs": 2,
+  "tails": 2,
+  "friends": ["Jack", "Cici"]
+
+
+
+};
