d38c49440dd9d3fcea0b969a712a771adf631623
Desirea Holton <dezholton@gmail.com>
2019-05-15 21:38:34 +0200

    Code and comment for lesson 4.91

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+/*4.91: You can nest arrays as well as other objects inside of objects. You use the same style of chained bracket notation to access those nested arrays. */
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
