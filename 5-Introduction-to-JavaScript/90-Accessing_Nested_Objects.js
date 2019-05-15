d0bd6b3e08deb18d7a663856c54a40b727906c27
Desirea Holton <dezholton@gmail.com>
2019-05-15 21:18:25 +0200

    Code and comment for lesson 4.90

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+/*4.90: You can get to nested layers of properties by chaining the dot/bracket notation.  */
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
