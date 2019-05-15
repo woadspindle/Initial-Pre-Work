6244ae81cc6d18592f9099a98eda7f6b7f76ed9e
Desirea Holton <dezholton@gmail.com>
2019-05-15 23:31:10 +0200

    Code and comment for lesson 4.102

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+/*4.102: If you multiply Math.random you can generate4 random whole numbers, too.  */
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+var randomNumberBetween0and9 = Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10);
+}
