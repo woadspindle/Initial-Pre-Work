7085a3791648fc0de5a7209f85a4af63a659d6c6
Desirea Holton <dezholton@gmail.com>
2019-05-15 20:07:24 +0200

    Code and comment for lesson 4.84

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,23 @@
+/*4.84: Obejects in JS are mutable. You update them with either dot or bracket notation. */
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog["name"] = "Happy Coder";
