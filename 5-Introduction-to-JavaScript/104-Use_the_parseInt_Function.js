b277daf10376b30639e622a030083c8baf9348eb
Desirea Holton <dezholton@gmail.com>
2019-05-15 23:45:01 +0200

    Code and comment for lesson 4.104

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+/*4.104: parseInt() looks at a string and changesit into an integer. If it can't be changed into number it returns NaN(Not a Number). */
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
