55f0fccfb869f39a9d920f7345eb2ccd81da4b13
Desirea Holton <dezholton@gmail.com>
2019-05-15 22:20:20 +0200

    Code and comment for lesson 4.96

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
 
+/*4.96: A for loop can count backwards if you decrement the final expression. */
+
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > -1; i -= 2) {
+  myArray.push(i);
+}
