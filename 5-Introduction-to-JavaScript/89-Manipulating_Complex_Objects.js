5f1f47f489c50820f08f768685f13b8731bc9140
Desirea Holton <dezholton@gmail.com>
2019-05-15 21:11:31 +0200

    Code and comment for lesson 4.89

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+/*4.89: Objects can handle multiple combinations of data types. This makes them a flexible data structure. Objects use a key/value format. */
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Elton John",
+    "title": "Madman Across the Water",
+    "release_year": 1971,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+
+  }
+];
