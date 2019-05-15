711bc189330c02c5142765542f944ac223d54d1b
Desirea Holton <dezholton@gmail.com>
2019-05-15 22:15:54 +0200

    Code and comment for lesson 4.95

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,17 @@
+/* 4.95: You can change the way a for loop counts by altering the final expression. You can changeit so that it counts by even or odd numbers. */
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 10; i += 2) {
+    myArray.push(i);
+}
