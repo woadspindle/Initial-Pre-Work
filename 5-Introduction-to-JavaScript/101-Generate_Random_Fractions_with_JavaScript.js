a88daa118f57b6fffceccce9f53aae815f534abd
Desirea Holton <dezholton@gmail.com>
2019-05-15 23:15:15 +0200

    Code and comment for lesson 4.101

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+/*4.101: Math.random will return a random decimal number between 0 and 1. */
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
