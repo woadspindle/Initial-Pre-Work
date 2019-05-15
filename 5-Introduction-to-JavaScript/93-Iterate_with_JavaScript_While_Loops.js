124ef0b8d389afe0ce67f876466761223f39e920
Desirea Holton <dezholton@gmail.com>
2019-05-15 21:58:55 +0200

    Code and comment for lesson 4.93

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+/**4.93: While loops run the same code while a given condition remains true. The stop when the condition is not true. */
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var i =0;
+while (i < 5){
+  myArray.push(i);
+  i++;
+}
