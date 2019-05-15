1c2f9fee67724891fe3e8d7784090d3835703c58
Desirea Holton <dezholton@gmail.com>
2019-05-15 19:58:04 +0200

    Code and comment for lesson 4.83

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+/*4.83: You can use bracket notation to search through properties by accessing ones stored as variable values in the object.  */
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
