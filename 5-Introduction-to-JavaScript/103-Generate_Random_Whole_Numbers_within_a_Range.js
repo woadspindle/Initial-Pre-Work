ef17ca50b65298e8b5f05af2098ca943e2a1ddf1
Desirea Holton <dezholton@gmail.com>
2019-05-15 23:38:42 +0200

    Code and comment for lesson 4.103

---
+++ 5-Introduction-to-JavaScript/103-Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,20 @@
+/*4.103: You can use min/max to generate a random number between two specific numbers. */
+
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
