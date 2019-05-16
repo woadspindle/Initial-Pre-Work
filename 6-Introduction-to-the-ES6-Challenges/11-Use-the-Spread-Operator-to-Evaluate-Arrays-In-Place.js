f7862939c9ac47b31d75121771bd1b2115ab3e39
Desirea Holton <dezholton@gmail.com>
2019-05-16 16:59:17 -0400

    Code and comment for lesson 5.11

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+/*5.11: Spread operator extends expressions like arrays to allow for multiple parameters or elements. */
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
