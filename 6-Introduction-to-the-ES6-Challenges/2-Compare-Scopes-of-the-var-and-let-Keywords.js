4423bc1467f70084b3d0084bcd9582cca4e110ef
Desirea Holton <dezholton@gmail.com>
2019-05-16 14:15:31 -0400

    Code and comment for lesson 5.2

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+/*5.2: If you declare let inside of a block, statement or expression it's scope will be limited to local. */
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
