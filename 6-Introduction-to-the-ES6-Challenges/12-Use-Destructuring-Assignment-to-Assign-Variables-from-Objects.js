ad81a3a5a5f3d420b9394d3ef1db31d101a034f5
Desirea Holton <dezholton@gmail.com>
2019-05-16 17:14:00 -0400

    code and comment for lesson 5.12

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,16 @@
+/*5.12:You can directly assign values to variables from objects with the destructuring assignment.Instead of individually declaring each variable you can create a kind of array of them to be called on. */
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow:tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
