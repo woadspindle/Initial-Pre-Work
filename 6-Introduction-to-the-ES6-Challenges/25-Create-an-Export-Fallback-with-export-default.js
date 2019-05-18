721fd04a880a86850188e947c463369db3ba4bd6
Desirea Holton <dezholton@gmail.com>
2019-05-18 15:00:58 -0600

    Code and comment for lesson 5.25

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,4 @@
+/*5.25: Export default is used when only one value isbeing exported from a file or to create a fallback value. */
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
