2c51b6be100d07d906f77535152e4e5133e0e058
Desirea Holton <dezholton@gmail.com>
2019-05-18 14:51:00 -0600

    Code and comment for lesson 5.22

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+/*5.22: THe import keyword allows you to selectively import part of a function library instead of the whole thing. */
+
+"use strict";
+import { capitalizeString } from "string_functions";
+capitalizeString("hello!");
