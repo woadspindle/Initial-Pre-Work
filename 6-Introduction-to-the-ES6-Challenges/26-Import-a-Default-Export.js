59d4891b23b89b341b55977ed9da747cad15e7c7
Desirea Holton <dezholton@gmail.com>
2019-05-18 15:04:41 -0600

    Code and comment for lesson 5.26

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,5 @@
+/*5.26: If you use export default you also have to use "import ___", not enclosed in curly braces, instead of the regular import syntax.  */
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
