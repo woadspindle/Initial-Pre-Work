4c7f16d14d59785c6d0e5cf9296c13a98c3a28b3
Desirea Holton <dezholton@gmail.com>
2019-05-18 14:28:30 -0600

    Code and comment for lesson 5.19

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+/*5.19: In ES6 you can leave the function keyword out when you create them inside of objects. */
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
