669815aa3b2a82e546a3acb25c60588feae5a633
Desirea Holton <dezholton@gmail.com>
2019-05-18 14:23:49 -0600

    Code and comment for lesson 5.18

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,13 @@
+/*5.18: Object literals are another way of writing more succinct code. THey help minimize global variables and can be of any data type. */
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
